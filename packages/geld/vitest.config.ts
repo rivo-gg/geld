67c89f71a0edb44732789ef5d9ffb1d1f29385a6
Dominik Koch <dominik@koch-bautechnik.de>
2025-06-06 22:48:10 +0200

    Refactor code structure for improved readability and maintainability

--- packages/geld/vitest.config.ts
+++ packages/geld/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    include: ['packages/geld/test/*.test.{js,ts,tsx}'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json-summary', 'html'],

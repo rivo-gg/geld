f7733978bbaa740a2cb6e32e300d6e7848e98a8a
Dominik Koch <dominik@koch-bautechnik.de>
2025-06-06 22:49:20 +0200

    revert: spesific test thing

--- packages/geld/vitest.config.ts
+++ packages/geld/vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['packages/geld/test/*.test.{js,ts,tsx}'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json-summary', 'html'],

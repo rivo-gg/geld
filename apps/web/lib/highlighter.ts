11ea048df77ff45455b47a0483eeb3a1652a1ad2
Dominik Koch <dominik@koch-bautechnik.de>
2025-06-07 21:53:10 +0300

    fix: include proper path in dockerfile

--- apps/web/lib/highlighter.ts
+++ apps/web/lib/highlighter.ts
@@ -1,13 +1,7 @@
 import { createHighlighter } from "shiki"
 
-let highlighterPromise: Promise<any> | null = null;
 
-export async function getHighlighter() {
-  if (!highlighterPromise) {
-    highlighterPromise = createHighlighter({
-      themes: ["catppuccin-frappe"],
-      langs: ["typescript", "bash", "shellscript", "json", "javascript", "python"]
-    });
-  }
-  return highlighterPromise;
-}
+export const highlighter = await createHighlighter({
+  themes: ["catppuccin-frappe"],
+  langs: ["typescript", "bash", "shellscript", "json", "javascript", "python"]
+})
